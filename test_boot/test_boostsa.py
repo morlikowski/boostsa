13e27047277877da4c57e70c701af54f268997ab
Matthias Orlikowski <morlikowski@techfak.uni-bielefeld.de>
2023-02-09 19:54:23 +0100

    Add p values test for feed

--- test_boot/test_boostsa.py
+++ test_boot/test_boostsa.py
@@ -27,6 +27,24 @@ def test_return_p_values_soft_labels():
     assert all(df_tot['p_sim'].isna() | ((df_tot['p_sim'] >= 0.0) & (df_tot['p_sim'] <= 1.0)))
     assert all(df_tot['p_cor'].isna() | ((df_tot['p_cor'] >= 0.0) & (df_tot['p_cor'] <= 1.0)))
 
+def test_return_p_values_feed_hard_labels():
+    boot = Bootstrap()
+    boot.feed(h0='h0',          exp_idx='h0.0', preds='test_boot/h0.0/preds.txt', targs='test_boot/h0.0/targs.txt', idxs='test_boot/h0.0/idxs.txt')
+    boot.feed(h0='h0',          exp_idx='h0.1', preds='test_boot/h0.1/preds.txt', targs='test_boot/h0.1/targs.txt', idxs='test_boot/h0.1/idxs.txt')
+    boot.feed(h0='h0', h1='h1', exp_idx='h1.0', preds='test_boot/h1.0/preds.txt', targs='test_boot/h1.0/targs.txt', idxs='test_boot/h1.0/idxs.txt')
+    boot.feed(h0='h0', h1='h1', exp_idx='h1.1', preds='test_boot/h1.1/preds.txt', targs='test_boot/h1.1/targs.txt', idxs='test_boot/h1.1/idxs.txt')
+    
+    df_tot, _ = boot.run(n_loops=100, sample_size=.2, verbose=True)
+    assert 'p_f1' in df_tot.columns
+    assert 'p_acc' in df_tot.columns
+    assert 'p_prec' in df_tot.columns
+    assert 'p_rec' in df_tot.columns
+    # p values should be NaN or float values between 0.0 and 1.0
+    assert all(df_tot['p_f1'].isna() | ((df_tot['p_f1'] >= 0.0) & (df_tot['p_f1'] <= 1.0)))
+    assert all(df_tot['p_acc'].isna() | ((df_tot['p_acc'] >= 0.0) & (df_tot['p_acc'] <= 1.0)))
+    assert all(df_tot['p_prec'].isna() | ((df_tot['p_prec'] >= 0.0) & (df_tot['p_prec'] <= 1.0)))
+    assert all(df_tot['p_rec'].isna() | ((df_tot['p_rec'] >= 0.0) & (df_tot['p_rec'] <= 1.0)))
+
 def test_all():
     boot = Bootstrap()
     boot.test(targs='test_boot/h0.0/targs.txt', h0_preds='test_boot/h0.0/preds.txt', h1_preds='test_boot/h1.0/preds.txt')
